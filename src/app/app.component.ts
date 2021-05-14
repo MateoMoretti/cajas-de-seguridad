7f24fd40d0f8b4f204f1fd4c30e089eedf296bcd
Mateo Moretti <mateomoretti@gmail.com>
2021-05-14 11:49:53 -0300

    Cambio fondo + modal

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,42 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'cajas-de-seguridad';
+
+  modal:any;
+
+  constructor() {}
+
+  ngOnInit(): void {
+
+    // Get the modal
+    
+    this.modal = document.getElementById("myModal");
+
+    // Get the button that opens the modal
+    var btn = document.getElementById("myBtn");
+
+    // Get the <span> element that closes the modal
+    var span = document.getElementsByClassName("close")[0];
+
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function(event) {
+      if (event.target == this.modal) {
+        this.modal.style.display = "none";
+      }
+    } 
+  }
+
+
+  abrir_ocupado(nombre:string, vencimiento:string){
+    this.modal.style.display = "block";
+    var name = document.getElementById("nombre");
+    name.textContent = nombre;
+  }
+
+  cerrar() {
+    this.modal.style.display = "none";
+  }
+
 }
+
+
